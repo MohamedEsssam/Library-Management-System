b2ce3cf8bd2bf7302d046a3064e721b80024035d
MohamedEsssam <me547010@gmail.com>
2023-11-08 10:54:12 +0200

    feat: handle routing

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import 'module-alias/register';
 import 'reflect-metadata';
 import express from 'express';
+import {
+  createExpressServer,
+  useContainer as rcUseContainer,
+} from 'routing-controllers';
+import { Container } from 'typedi';
 
 import '@configs/ormconfig';
 import { expressEnv } from '@utils/environments';
+import { join } from 'path';
 
 async function bootstrap() {
+  rcUseContainer(Container);
   const port = expressEnv['PORT'];
-  const app = express();
-
-  app.use(express.json());
+  const app = createExpressServer({
+    routePrefix: '/api',
+    controllers: [join(__dirname + '/modules/**/*.controller.ts')],
+  });
   app.listen(port ?? 3000, () => {
     console.log('Server is listening on port 3000');
   });
